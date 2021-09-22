fc5995e70341f9bf54251e89950719bb9c521db4
natwPine <natw.mezza@hotmail.fr>
2021-09-22 09:07:50 +0200

    NMezza - [SetUP] Add folder                  [Plug-in] add DoTween, ProGrid, ProBuilder

---
+++ Assets/01_Scripts/00_Manager/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+            Debug.LogWarning("Multiple instance of same Singleton : GameManager");
+        else
+            instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
