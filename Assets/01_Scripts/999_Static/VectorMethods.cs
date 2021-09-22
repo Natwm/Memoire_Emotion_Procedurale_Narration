2834a84027586cb29bf5070bd479ced6d8140ee5
natwPine <natw.mezza@hotmail.fr>
2021-09-22 09:54:36 +0200

    NMezza - [Feature]Static class for calcul distance entre 2 vector

---
+++ Assets/01_Scripts/999_Static/VectorMethods.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public static class VectorMethods 
+{
+    public static bool CompareVectorApproximatelyByDistance2D(Vector2 firstVector, Vector2 secondVector, float allowedDifference)
+    {
+        float distanceX = firstVector.x - secondVector.x;
+        float distanceY = firstVector.y - secondVector.y;
+
+        if (Mathf.Abs(distanceX) > allowedDifference)
+            return false;
+
+        if (Mathf.Abs(distanceY) > allowedDifference)
+            return false;
+
+        return true;
+    }
+
+    public static bool CompareVectorApproximatelyByPercent2D(Vector2 firstVector, Vector2 secondVector, float percent)
+    {
+        float distanceX = firstVector.x - secondVector.x;
+        float distanceY = firstVector.y - secondVector.y;
+
+        if (Mathf.Abs(distanceX) > firstVector.x * percent)
+            return false;
+
+        if (Mathf.Abs(distanceY) > firstVector.y * percent)
+            return false;
+
+        return true;
+    }
+
+    public static bool CompareVectorApproximatelyByDistance(Vector3 firstVector, Vector3 secondVector, float allowedDifference)
+    {
+        float distanceX = firstVector.x - secondVector.x;
+        float distanceY = firstVector.y - secondVector.y;
+        float distanceZ = firstVector.z - secondVector.z;
+
+        if (Mathf.Abs(distanceX) > allowedDifference)
+            return false;
+
+        if (Mathf.Abs(distanceY) > allowedDifference)
+            return false;
+
+        return Mathf.Abs(distanceZ) >= allowedDifference; ;
+    }
+
+    public static bool CompareVectorApproximatelyByPercent(Vector3 firstVector, Vector3 secondVector, float percent)
+    {
+        float distanceX = firstVector.x - secondVector.x;
+        float distanceY = firstVector.y - secondVector.y;
+        float distanceZ = firstVector.z - secondVector.z;
+
+        if (Mathf.Abs(distanceX) > firstVector.x * percent)
+            return false;
+
+        if (Mathf.Abs(distanceY) > firstVector.y * percent)
+            return false;
+
+        return Mathf.Abs(distanceZ) >= firstVector.z * percent; ;
+    }
+}
