9a41a1d9a7510a68f2425ab404676921116e24df
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2021-09-24 12:09:39 +0200

    [Sprites] Ajout Gabarit de base

---
+++ Assets/FaceRandomiser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FaceRandomiser : MonoBehaviour
+{
+
+    public Image Mouth;
+    public int MouthIndex;
+    public Image LeftBrow;
+    public int LeftIndex;
+    public Image RightBrow;
+    public int RightIndex;
+    public Image FaceColor;
+    public int FaceIndex;
+
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FaceGeneration()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
