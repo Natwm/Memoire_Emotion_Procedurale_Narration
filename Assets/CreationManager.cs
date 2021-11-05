b24c8804b917d31d95bd90de8ac599f191f7af06
natwPine <natw.mezza@hotmail.fr>
2021-11-05 10:55:45 +0100

    NMezza - [Scene]Add test creation scene

---
+++ Assets/CreationManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationManager : MonoBehaviour
+{
+    public enum m_PenStatus
+    {
+        NONE,
+        FREEZE,
+        WANT,
+        DONT_WANT,
+        DRAW
+    }
+
+
+    public static CreationManager instance;
+
+    [SerializeField] private m_PenStatus m_Pen;
+
+    public m_PenStatus Pen { get => m_Pen; set => m_Pen = value; }
+
+    void Awake()
+    {
+        if (instance != null)
+            Debug.LogWarning("Multiple instance of same Singleton : CreationManager");
+        else
+            instance = this;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangePen(string usedPen)
+    {
+        switch (usedPen)
+        {
+            case "freeze":
+                if (Pen != m_PenStatus.FREEZE)
+                    Pen = m_PenStatus.FREEZE;
+                else
+                    Pen = m_PenStatus.NONE;
+                break;
+
+            case "want":
+                if (Pen != m_PenStatus.WANT)
+                    Pen = m_PenStatus.WANT;
+                else
+                    Pen = m_PenStatus.NONE;
+                break;
+
+            case "dont":
+                if (Pen != m_PenStatus.DONT_WANT)
+                    Pen = m_PenStatus.DONT_WANT;
+                else
+                    Pen = m_PenStatus.NONE;
+                break;
+
+            case "none":
+                    Pen = m_PenStatus.NONE;
+                break;
+
+            case "draw":
+                if (Pen != m_PenStatus.DRAW)
+                    Pen = m_PenStatus.DRAW;
+                else
+                    Pen = m_PenStatus.NONE;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+}
---
+++ Assets/characterCreation.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class characterCreation : MonoBehaviour
+{
+
+    private Button myButton;
+    public enum CharacterStatus
+    {
+        FREEZE,
+        WANT,
+        DONT_WANT,
+        NONE
+    }
+
+    [SerializeField] private CharacterStatus m_Status;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myButton = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AffectByPen()
+    {
+        
+        switch (CreationManager.instance.Pen)
+        {
+            case CreationManager.m_PenStatus.NONE:
+                myButton.image.color = Color.white;
+                m_Status = CharacterStatus.NONE;
+                break;
+
+            case CreationManager.m_PenStatus.FREEZE:
+                if(m_Status != CharacterStatus.FREEZE)
+                {
+                    myButton.image.color = Color.cyan;
+                    m_Status = CharacterStatus.FREEZE;
+                }
+                else
+                {
+                    myButton.image.color = Color.white;
+                    m_Status = CharacterStatus.NONE;
+                }
+                break;
+
+            case CreationManager.m_PenStatus.WANT:
+                if (m_Status != CharacterStatus.WANT)
+                {
+                    myButton.image.color = Color.green;
+                    m_Status = CharacterStatus.WANT;
+                }
+                else
+                {
+                    myButton.image.color = Color.white;
+                    m_Status = CharacterStatus.NONE;
+                }
+                break;
+
+            case CreationManager.m_PenStatus.DONT_WANT:
+                if (m_Status != CharacterStatus.DONT_WANT)
+                {
+                    myButton.image.color = Color.red;
+                    m_Status = CharacterStatus.DONT_WANT;
+                }
+                else
+                {
+                    myButton.image.color = Color.white;
+                    m_Status = CharacterStatus.NONE;
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+}
