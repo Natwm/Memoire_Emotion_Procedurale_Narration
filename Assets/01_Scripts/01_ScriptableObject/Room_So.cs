aa381ec5deebbd005814ba9b2e37923780fe1ea8
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2021-12-04 21:41:32 +0100

    [Feature] Room_So

---
+++ Assets/01_Scripts/01_ScriptableObject/Room_So.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Room", menuName = "New Scriptable Object/New Room")]
+public class Room_So : ScriptableObject
+{
+    [Header("Param")]
+    [SerializeField] private string m_RoomName;
+    [SerializeField] private Vector2Int m_Roomsize;
+    [SerializeField] private List<CaseContener_SO> m_containedObjects;
+
+    [Space]
+    [Header("Narration")]
+    [TextArea(10,50)]
+    [SerializeField] string m_IntroText;
+
+    [TextArea(10, 50)]
+    [SerializeField] string m_OutroText;
+
+    #region Getter && Setter
+
+    public string RoomName { get => m_RoomName; set => m_RoomName = value; }
+    public Vector2Int Room_Size { get => m_Roomsize; set => m_Roomsize = value; }
+    public List<CaseContener_SO> PossibleTiles { get => m_containedObjects; set => m_containedObjects = value; }
+    public string Room_IntroText { get => m_IntroText; set => m_IntroText = value; }
+    public string Room_OutroText { get => m_OutroText; set => m_OutroText = value; }
+
+    #endregion
+
+}
---
+++ Assets/RoomGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RoomGenerator : MonoBehaviour
+{
+    public Room_So TestRoom;
+    public static RoomGenerator instance;
+    
+    void Awake()
+    {
+        if (instance != null)
+            Debug.LogWarning("Multiple instance of same Singleton : EventGenerator");
+        else
+            instance = this;
+    }
+
+    public void GenerateRoom(Room_So RoomToCreate)
+    {
+        int distribution = (RoomToCreate.Room_Size.x * RoomToCreate.Room_Size.y) / 4;
+        EventGenerator.instance.ClearGrid();
+        GridManager.instance.CreateTerrainWithParam(TestRoom.Room_Size);
+        EventGenerator.instance.DetermineDoors(false);
+        EventGenerator.instance.PopulateTiles(distribution);
+        EventGenerator.instance.DetermineTypeFromRoom(RoomToCreate);
+
+
+        //Get Room List Of Obj
+        
+        //Spawn List on Obj
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateRoom(TestRoom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
