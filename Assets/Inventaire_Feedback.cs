91337ea65a80a3e0fbd88dd300e419cfd399280b
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2021-12-09 16:34:14 +0100

    [Feedback] Ajout Feedback Pour Stockage

---
+++ Assets/Inventaire_Feedback.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Inventaire_Feedback : MonoBehaviour
+{
+    SpriteRenderer render;
+    Color baseCol;
+    public void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+        baseCol = new Color(render.color.r, render.color.g, render.color.b, 1);
+    }
+
+    public void PlayStockageFeedback()
+    {
+
+        render.color = baseCol;
+        render.DOFade(0, 1f);
+    }
+
+    
+}
